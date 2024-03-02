b8f281e610c639c307b0206baf8ed21b6d818b95
Mustafas1998 <ms6534126@gmail.com>
2024-03-02 15:10:29 +0200

    Eventlisnterforlettes

--- scripts/game.js
+++ scripts/game.js
@@ -3,25 +3,68 @@ let max_guesses = 6;
 let letters_guessed = [];
 
 //random-word
-function random_word(){
+function random_word()
+{
     word = list[Math.floor(Math.random()*list.length)]
     alert(word);
-    
+
 }
+
 random_word();
 
+function hang_update()
+{
+
+
+}
 
 //display-word-section
-function display_dashes_section(){
+function display_dashes_section()
+{
     let dashes = "";
     for(i=0; i<word.length; i++){
         dashes += "_";
     }
     let answer = document.querySelector("#answer-section");
-    answer.textContent = dashes.split('').join(" ");
+    answer.innerHTML = dashes.split('').join(" ");
 }
 
 display_dashes_section();
+
+
+//event-listner for  letters
+
+let letter_clicks = document.querySelectorAll(".letter");
+for(let i in letter_clicks)
+{   let letter = letter_clicks[i];
+    letter.addEventListener("click", function()
+    {
+        // if(s)
+        // {
+        alert('click')
+        // } else if(max_guesses<=0){
+        //     alert(`GAMEOVER.Word is ${word}`)
+        //     alert("clicks=0")
+        // }
+            
+    });
+};
+
+
+   
+
+    
+ 
+
+
+
+
+
+//check letter
+// function check_letter(letter_answer)
+// {
+
+// }
 
 
 
